11016052e6b30472c209801d3883f14b3ccb4f90
jackoyugi <oyugijack@gmail.com>
2020-07-15 12:22:33 +0300

    added new database and its rule, save, update and delete tests and function

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.sql2o.Sql2o;
 
@@ -12,6 +13,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AnimalTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void instantiatesCorrectly_true(){
@@ -30,25 +34,62 @@ public class AnimalTest {
         assertEquals("owl", animal.getName());
     }
 
-//    @Test
-//    public void Animal_instantiatesCorrectlyWithAnId() {
-//        Animal animal = new Animal("owl");
-//        animal.save();
-//        assertTrue(animal.getId()>0);
-
-
-
-
-
     @Test
-    public void save() {
+    public void Animal_instantiatesCorrectlyWithAnId() {
+        Animal animal = new Animal("owl");
+        animal.save();
+        assertTrue(animal.getId() > 0);
     }
 
     @Test
-    public void update() {
+    public void save_CorrectlyIntoTheDatabase() {
+        Animal animal = new Animal("owl");
+        animal.save();
+        assertTrue(Animal.all().get(0).equals(animal));
+    }
+    @Test
+    public void findById_returnsAnimalWIthSameID_secondAnimal(){
+        Animal animalOne = new  Animal("owl");
+        animalOne.save();
+        Animal animalTwo = new Animal("crown");
+        animalTwo.save();
+        assertEquals(Animal.findById(animalTwo.getId()), animalTwo);
+    }
+    @Test
+    public void equals_returnsTrueIfAnimalsAreSame(){
+        Animal animalOne = new Animal("owl");
+        Animal animalTwo = new Animal("owl");
+        assertTrue(animalOne.equals(animalTwo));
+    }
+    @Test
+    public void save_returnsTrueIfNamesAreTheSame(){
+        Animal animal = new Animal("owl");
+        animal.save();
+        assertEquals(Animal.all().get(0), animal);
+    }
+    @Test
+    public void all_returnsAllInstancesOfAnimals_true(){
+        Animal animalOne = new  Animal("owl");
+        animalOne.save();
+        Animal animalTwo = new Animal("crown");
+        animalTwo.save();
+        assertEquals(Animal.all().get(0), animalOne);
+        assertEquals(Animal.all().get(1), animalTwo);
+    }
+    @Test
+    public void save_assignsIdToObject() {
+        Animal animal = new Animal("owl");
+        animal.save();
+        Animal savedAnimal = Animal.all().get(0);
+        assertEquals(animal.getId(), savedAnimal.getId());
+    }
+    @Test
+    public void delete_deletesAnimal_true(){
+        Animal animal = new Animal("Chui");
+        animal.save();
+        int animalId = animal.getId();
+        animal.delete();
+        assertEquals(null, Animal.findById(animalId));
     }
 
-    @Test
-    public void delete() {
-    }
 }
