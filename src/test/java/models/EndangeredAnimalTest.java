7848db70ad697b3dfc869cddf3190a48d6958227
jackoyugi <oyugijack@gmail.com>
2020-07-15 13:05:25 +0300

    added test and function for getting name and type of the endangered

--- src/test/java/models/EndangeredAnimalTest.java
+++ src/test/java/models/EndangeredAnimalTest.java
@@ -9,8 +9,35 @@ public class EndangeredAnimalTest {
     @Rule
     public DatabaseRule database = new DatabaseRule();
 
+    @Test
+    public void enAnimal_InstantiatesCorrectly_true(){
+        EndangeredAnimal endangeredAnimal = setUpTheTask();
+        assertEquals(true,endangeredAnimal instanceof EndangeredAnimal);
+    }
+
+    @Test
+    public void enAnimal_InstantiatesWithNameCorrectly_String(){
+        EndangeredAnimal endangeredAnimal = setUpTheTask();
+        assertEquals("monkey",endangeredAnimal.getName());
+    }
+    @Test
+    public void enAnimal_CorrectlyInstantiatesWithType() throws Exception{
+        EndangeredAnimal endangeredAnimal = setUpTheTask();
+        assertEquals("endangered",endangeredAnimal.getType());
+    }
+    @Test
+    public void enAnimal_InstantiatesCorrectlyWithId_Int()throws Exception{
+        EndangeredAnimal endangeredAnimal = setUpTheTask();
+        endangeredAnimal.saveEn();
+        assertTrue(endangeredAnimal.getId()>0);
+    }
+
 
     @Test
     public void saveEn() {
+    }
+
+    public EndangeredAnimal setUpTheTask(){
+        return new EndangeredAnimal("monkey",EndangeredAnimal.ADULT,EndangeredAnimal.ILL);
     }
 }
