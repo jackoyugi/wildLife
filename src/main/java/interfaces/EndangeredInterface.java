29ca31ef0dd72a4885fa4bef40b8030c380e2409
jackoyugi <oyugijack@gmail.com>
2020-07-15 12:54:04 +0300

    added class endangered animal and its corresponding properties

---
+++ src/main/java/interfaces/EndangeredInterface.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import models.Animal;
+import models.EndangeredAnimal;
+
+import java.util.List;
+
+public interface EndangeredInterface {
+    //SAVE
+    void saveEn();
+
+    //LIST ALL
+
+}
--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,4 +1,31 @@
 package models;
 
-public class EndangeredAnimal {
+import interfaces.EndangeredInterface;
+
+public class EndangeredAnimal extends Animal implements EndangeredInterface {
+    private String health;
+    private String age;
+
+    public static final String ANIMAL_TYPE = "endangered";
+    public static final String ADULT = "adult";
+    public static final String YOUNG = "youthful";
+    public static final String NEWBORN = "newborn";
+    public static final String HEALTHY = "very healthy";
+    public static final String AVERAGE = "averagely unhealthy";
+    public static final String ILL = "very unhealthy";
+
+
+    public EndangeredAnimal(String name, String age, String health) {
+        super(name);
+        this.type = ANIMAL_TYPE;
+        this.health = health;
+        this.age = age;
+
+    }
+
+
+    @Override
+    public void saveEn() {
+
+    }
 }
