4dd9f7d066a89ab1bd51acfde844dfa1f39c12be
jackoyugi <oyugijack@gmail.com>
2020-07-15 12:28:03 +0300

    testing of animal route through the user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,49 @@
+import models.Animal;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        Integer port;
+
+        if (processBuilder.environment().get("PORT") != null) {
+            port = Integer.parseInt(processBuilder.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/animals/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("animals", Animal.ANIMAL_TYPE);
+            return new ModelAndView(model, "animal-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            Animal animal = new Animal(name);
+            animal.save();
+            model.put("animals", Animal.all());
+            return new ModelAndView(model, "animals.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("animals", Animal.all());
+            return new ModelAndView(model, "animals.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
